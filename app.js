458cda147a84a1719696c4cbcc806401b30280bf
Kaan Yilmaz <douscriptist@gmail.com>
2020-05-29 03:29:19 +0300

    init for starting electron template

---
+++ app.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const os = require('os');
+const { ipcRenderer } = require('electron');
---
+++ main.js
@@ -0,0 +1,70 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const slash = require('slash');
+const log = require('electron-log');
+
+const isDevMode = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
+
+function createMainWindow() {
+	mainWindow = new BrowserWindow({
+		title: 'SysHow',
+		width: isDevMode ? 750 : 500,
+		height: 600,
+		icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+		resizable: isDevMode,
+		backgroundColor: '#252b4d',
+		webPreferences: {
+			nodeIntegration: true,
+			// enableRemoteModule: true, // electron-log
+		},
+	});
+
+	// Is development mode open developer tools default
+	isDevMode && mainWindow.webContents.openDevTools();
+
+	mainWindow.loadFile(`${__dirname}/app/index.html`);
+}
+
+app.on('ready', () => {
+	createMainWindow();
+
+	const mainMenu = Menu.buildFromTemplate(menu);
+	Menu.setApplicationMenu(mainMenu);
+
+	mainWindow.on('closed', () => {
+		mainWindow = null;
+	});
+});
+
+const menu = [
+	// For mac file option
+	...(isMac ? [{ role: 'appMenu' }] : []),
+	{ role: 'fileMenu' },
+	...(isDevMode
+		? [
+				{
+					label: 'Development',
+					submenu: [
+						{ role: 'reload' },
+						{ role: 'forcereload' },
+						{ type: 'separator' },
+						{ role: 'toggledevtools' },
+					],
+				},
+		  ]
+		: []),
+];
+
+app.on('window-all-closed', () => {
+	if (!isMac) {
+		app.quit();
+	}
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createMainWindow();
+	}
+});
