61e48737eb7ff443c8cdfe5adbdf362dbe1b737d
Kaan Yilmaz <douscriptist@gmail.com>
2020-05-29 18:57:47 +0300

    system info & some ui functionals

---
+++ app/js/monitor.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const osCore = require('os');
+const osu = require('node-os-utils');
+
+const os = osu.os;
+const cpu = osu.cpu;
+const mem = osu.mem;
+
+// Select DOM Elements
+const cpuModel = document.getElementById('cpu-model');
+const cpuUsage = document.getElementById('cpu-usage');
+const cpuFree = document.getElementById('cpu-free');
+const ramUsage = document.getElementById('memory-usage');
+const ramFree = document.getElementById('memory-free');
+const compName = document.getElementById('comp-name');
+const compUser = document.getElementById('user');
+const compOS = document.getElementById('os');
+const compIP = document.getElementById('ip');
+const memory = document.getElementById('mem-total');
+const sysUptime = document.getElementById('sys-uptime');
+
+// Run each seconds - Interval
+setInterval(() => {
+	// CPU Usage
+	cpu.usage().then((cpuInfo) => {
+		cpuUsage.innerText = `${cpuInfo.toFixed(2)} %`;
+	});
+	// CPU Free
+	cpu.free().then((cpuInfo) => {
+		cpuFree.innerText = `${cpuInfo.toFixed(2)} %`;
+	});
+
+	// RAM Usage
+	mem.info().then((memInfo) => {
+		// ramUsage.innerText = memInfo.usedMemMb + ' GB';
+		ramUsage.innerText = `${memInfo.usedMemMb} GB - ${(
+			100 - memInfo.freeMemPercentage
+		).toFixed(2)} %`;
+		ramFree.innerText = `${
+			memInfo.freeMemMb
+		} GB - ${memInfo.freeMemPercentage.toFixed(2)} %`;
+	});
+
+	// Uptime
+	sysUptime.innerText = formatDate(os.uptime());
+}, 1500);
+
+// Set cpu model
+cpuModel.innerText = cpu.model();
+
+// Computer Name
+compName.innerText = os.hostname();
+
+// Username
+compUser.innerText = osCore.userInfo().username;
+
+// OS
+compOS.innerText = `${getOS(os.type())} ${osCore.release()} (${os.arch()})`; // 'Linux' on Linux, 'Darwin' on macOS, and 'Windows_NT' on Windows
+
+// IP
+compIP.innerText = os.ip();
+
+// Total Memory
+mem.info().then((info) => {
+	memory.innerText = `${info.totalMemMb} GB`;
+});
+
+// OS Type Normalizer
+function getOS(osType) {
+	switch (osType) {
+		case 'Windows_NT':
+			return 'Windows';
+		case 'Darwin':
+			return 'macOS';
+		case 'Linux':
+			return 'Linux';
+		default:
+			return 'undefined';
+	}
+}
+
+// Custom Date Format
+//
+function formatDate(time) {
+	time = +time;
+	const day = Math.floor(time / (3600 * 24));
+	const hour = Math.floor((time % (3600 * 24)) / 3600);
+	const mins = Math.floor((time % 3600) / 60);
+	const seconds = Math.floor(time % 60);
+
+	return `${day}d, ${hour}h, ${mins}m, ${seconds}s`;
+}
--- app/js/tabs.js
+++ app/js/tabs.js
@@ -1,27 +1,44 @@
-const contents = document.querySelectorAll('.content')
-const listItems = document.querySelectorAll('nav ul li')
+const contents = document.querySelectorAll('.content');
+const listItems = document.querySelectorAll('nav ul li');
+const eyeIcon = document.getElementById('eyeIcon');
+const ip = document.getElementById('ip');
+
+let isShown = false;
+
+// Showin ip adress
+eyeIcon.addEventListener('click', () => {
+	ip.classList.toggle('blur');
+	if (!isShown) {
+		eyeIcon.classList.remove('fa-eye');
+		eyeIcon.classList.add('fa-eye-slash');
+	} else {
+		eyeIcon.classList.add('fa-eye');
+		eyeIcon.classList.remove('fa-eye-slash');
+	}
+	isShown = !isShown;
+});
 
 listItems.forEach((item, index) => {
 	item.addEventListener('click', () => {
-		hideAllContents()
-		hideAllItems()
+		hideAllContents();
+		hideAllItems();
 
 		// activate list item
-		item.classList.add('active')
+		item.classList.add('active');
 
 		// display content
-		contents[index].classList.add('show')
-	})
-})
+		contents[index].classList.add('show');
+	});
+});
 
 function hideAllContents() {
 	contents.forEach((content) => {
-		content.classList.remove('show')
-	})
+		content.classList.remove('show');
+	});
 }
 
 function hideAllItems() {
 	listItems.forEach((item) => {
-		item.classList.remove('active')
-	})
+		item.classList.remove('active');
+	});
 }
